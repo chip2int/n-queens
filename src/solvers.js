3aec9eb236f48273f5c92e835976195ed2e3bd0d
Hack Reactor Students <jdtoney@gmail.com>
2013-11-20 19:31:32 -0800

    Added NRook one solution

--- src/solvers.js
+++ src/solvers.js
@@ -13,8 +13,45 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 window.findNRooksSolution = function(n){
-  var solution = undefined; //fixme
+  var solution = []; //fixme
+  var rowArr = [];
+  var colArr = [];
 
+  for (var row = 0; row < n; row++) {
+    var colArrForRow = Array(n);
+
+    for (var i = 0; i < n; i++) {
+     colArrForRow[i] = 0;
+    }
+    solution[row] = colArrForRow;
+  }
+
+  for (var i = 0; i < n; i++) {
+    rowArr[i] = colArr[i] = i;
+  }
+
+  var placeRooks = function(rowArrOrig, colArrOrig) {
+    var rowArr = rowArrOrig;
+    var colArr = colArrOrig;
+    
+    if (rowArr.length === 1 && colArr.length === 1) {
+      solution[rowArr[0]][colArr[0]]=1;
+      return;
+    }
+
+    var rowIdx = (Math.floor(Math.random()* 10) %rowArr.length);
+    var colIdx = (Math.floor(Math.random()* 10) %colArr.length);
+
+    var randomRowVal = rowArr[rowIdx];
+    var randomColVal = colArr[colIdx];
+
+    solution[randomRowVal][randomColVal] = 1;
+    rowArr.splice(rowIdx, 1);
+    colArr.splice(colIdx, 1);
+    placeRooks(rowArr, colArr);
+  };
+   
+  placeRooks(rowArr, colArr);
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
